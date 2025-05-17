18ec69392b782e92f3652cd8aa006690ea2dac2e
AlejandroVolkova <120342552+AlejandroVolkova@users.noreply.github.com>
2025-05-17 19:22:36 -0400

    Update extra.sh

--- extra.sh
+++ extra.sh
@@ -25,21 +25,9 @@ yay -S fd fzf ripgrep bat neofetch jq xclip gpaste zoxide --noconfirm
 echo "Installing Git..."
 yay -S github-cli git-delta lazygit --noconfirm
 
-# Install Tmux
-echo "Installing Tmux..."
-yay -S tmux --noconfirm
-
-# Install Fish shell
-echo "Installing Fish shell..."
-yay -S fish --noconfirm
-
 # Install Firefox
 echo "Installing Firefox..."
 yay -S firefox --noconfirm
-
-# Install i3
-echo "Installing i3..."
-sudo pacman -Sy xorg xorg-xinit i3-gaps i3lock i3status i3blocks dmenu dex xss-lock rofi polybar feh lxappearance
 
 # Install rofi theme
 git clone --depth=1 https://github.com/adi1090x/rofi.git
